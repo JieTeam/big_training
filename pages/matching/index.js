159b56ad65ed6857ce9b2f0bd82171a20af3e511
yckj3156 <zhaojunjie@cloudwalk.com>
2020-09-19 16:54:48 +0800

    匹配对战

---
+++ pages/matching/index.js
@@ -0,0 +1,75 @@
+// pages/matching/index.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        hint: "匹配中...",
+        againstFlag: false,
+        meInfo: {
+            header: "../../assets/images/test/logo.jpg",
+            name: "水木青蓝"
+        },
+        rivalInfo: {
+            header: "../../assets/images/test/me_logo.png",
+            name: "小白"
+        }
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
