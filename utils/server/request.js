c25238320caf488ea8fc0bb1ccf01671a9d07c51
yckj3156 <zhaojunjie@cloudwalk.com>
2020-09-28 23:35:08 +0800

    排行榜

--- utils/server/request.js
+++ utils/server/request.js
@@ -1,6 +1,10 @@
 import Api from "./api";
 import { baseUrl } from './config'
 
+/**
+ * 每周一测
+ */
+
 // 抽取每周一测题目
 export const getWeektoPicApi = (params) => {
     return Api.request('POST', `${baseUrl}/bt/training/week/extractQuestion?userId=${params.userId}`, params);
@@ -9,4 +13,17 @@ export const getWeektoPicApi = (params) => {
 // 提交每周一测答题
 export const subWeekAnswerApi = (params) => {
     return Api.request('POST', `${baseUrl}/bt/training/week/submit`, params);
+}
+
+
+/**
+ * 排行榜
+ * params { 
+ *  type: 1个人 2省 3市 4县 
+ * }
+ */
+
+// 获取答题人展示排行榜信息
+export const getRankListApi = (params) => {
+    return Api.request('POST', `${baseUrl}/bt/rank/getRankList?type=${params.type}`);
 }
