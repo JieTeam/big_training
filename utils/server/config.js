cfee8e386cd31078c693e8c778996b74fa7369e0
yckj3156 <zhaojunjie@cloudwalk.com>
2020-09-28 16:37:09 +0800

    修改服务地址

--- utils/server/config.js
+++ utils/server/config.js
@@ -4,6 +4,6 @@ const production = true;
 // const testUrl = 'http://3435k69g44.zicp.vip';
 const testUrl = 'http://192.168.1.4:8090';
 
-const prdUrl = 'http://125.35.101.176:9001';
+const prdUrl = 'http://125.35.101.176:8090';
 
 export const baseUrl = production ? prdUrl : testUrl;
--- utils/util.js
+++ utils/util.js
@@ -53,7 +53,7 @@ module.exports = {
         // wsUrl: `ws://10.134.62.106:8090/websocket`, // 焦公司服务
         // wsUrl: `ws://3435k69g44.zicp.vip/websocket`, // 焦家服务
         // wsUrl: `ws://192.168.1.4:8090/websocket`, // 本机服务
-        wsUrl: `ws://125.35.101.176:9001/websocket`, // 测试服务
+        wsUrl: `ws://125.35.101.176:8090/websocket`, // 测试服务
     },
     request: request,
     throttle: throttle,
