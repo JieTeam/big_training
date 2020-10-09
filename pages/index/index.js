e0962be4d0afc871fedbea56c10bcf34817a674b
yckj3156 <zhaojunjie@cloudwalk.com>
2020-10-09 20:25:38 +0800

    问题调整

--- pages/index/index.js
+++ pages/index/index.js
@@ -47,83 +47,83 @@ Page({
         break;
     }
     if (!url) return;
-    if(type=='fight'||type=='week') { // 匹配和每周一测页面重定向跳转，返回首页会重新加载获取战绩数据
-        wx.redirectTo({
-            url,
-        });
-    } else {
-        wx.navigateTo({
-            url,
-        });
-    }
+    wx.navigateTo({
+        url,
+    });
+    
   },
   rulesBoxShow() {
     this.setData({
       rulesBox: !this.data.rulesBox,
     });
   },
+  onShow: function () {
+        this.loadData();
+  },
   onLoad: function () {
     if (!app.globalData.userInfo || app.globalData.userInfo.login === false) {
-      wx.redirectTo({
+        wx.redirectTo({
         url: '/pages/loginType/loginType',
-      })
+        })
     } else if (app.globalData.userInfo.roleType === '1') {
-      wx.redirectTo({
+        wx.redirectTo({
         url: '/pages/adminIndex/adminIndex',
-      })
+        })
     }
+  },
+  loadData() {
     if (app.globalData.userInfo.login) {
-      let showHonor = false
-      const userInfo = app.globalData.userInfo
-      if (app.globalData.userInfo.roleType === '2') {
-        showHonor = false
-      } else {
-        showHonor = true
-      }
-      this.setData({
-        showHonor,
-      })
-      wx.showLoading({
-        title: '数据加载中...',
-      })
-      this.getUserInfo(userInfo.userId).then(res => {
-        console.log(res)
-        wx.hideLoading()
-        if(res.code === 1) {
-          const { userLevel, winRate, winCount,tieCount, loseCount, score, rank, phoneNo, id, name, 
-                    roleType, workingDivision, fullRegionName,likeCount, headUrl } = res.data
-          userInfo.userLevel = userLevel; // 等级
-          userInfo.winRate = (winRate * 100).toFixed(2); // 胜率
-          userInfo.winCount = winCount; // 胜利场次
-          userInfo.tieCount = tieCount; // 平局场次
-          userInfo.loseCount = loseCount; // 失败场次
-          userInfo.rank = rank; // 排名
-          userInfo.count = winCount + tieCount + loseCount;
-          userInfo.score = score;
-          userInfo.phoneNo = phoneNo;
-          userInfo.userId = id;
-          userInfo.likeCount = likeCount;
-          userInfo.name = name;
-          userInfo.workingDivision = workingDivision; // 所属区域代码
-          userInfo.fullRegionName = fullRegionName;
-          // 后台返回的是数字 转字符串
-          userInfo.roleType = roleType + '';
-          userInfo.avatarUrl = headUrl;
-
-          this.setData({
-            userInfo,
-            show: true,
-          })
+        let showHonor = false
+        const userInfo = app.globalData.userInfo
+        if (app.globalData.userInfo.roleType === '2') {
+          showHonor = false
         } else {
-          this.setData({
-            show: true,
-          })
+          showHonor = true
         }
-      }).catch(err => {
-        console.log('>>err', err)
-        wx.hideLoading()
-        this.setData({show: true,})
-      })
+        this.setData({
+          showHonor,
+        })
+        wx.showLoading({
+          title: '数据加载中...',
+        })
+        this.getUserInfo(userInfo.userId).then(res => {
+          console.log(res)
+          wx.hideLoading()
+          if(res.code === 1) {
+            const { userLevel, winRate, winCount,tieCount, loseCount, score, rank, phoneNo, id, name, 
+                      roleType, workingDivision, fullRegionName,likeCount, headUrl } = res.data
+            userInfo.userLevel = userLevel; // 等级
+            userInfo.winRate = (winRate * 100).toFixed(2); // 胜率
+            userInfo.winCount = winCount; // 胜利场次
+            userInfo.tieCount = tieCount; // 平局场次
+            userInfo.loseCount = loseCount; // 失败场次
+            userInfo.rank = rank; // 排名
+            userInfo.count = winCount + tieCount + loseCount;
+            userInfo.score = score;
+            userInfo.phoneNo = phoneNo;
+            userInfo.userId = id;
+            userInfo.likeCount = likeCount;
+            userInfo.name = name;
+            userInfo.workingDivision = workingDivision; // 所属区域代码
+            userInfo.fullRegionName = fullRegionName;
+            // 后台返回的是数字 转字符串
+            userInfo.roleType = roleType + '';
+            userInfo.avatarUrl = headUrl;
+  
+            this.setData({
+              userInfo,
+              show: true,
+            })
+          } else {
+            this.setData({
+              show: true,
+            })
+          }
+        }).catch(err => {
+          console.log('>>err', err)
+          wx.hideLoading()
+          this.setData({show: true,})
+        })
     }
   },
   getUserInfo(openId) {
