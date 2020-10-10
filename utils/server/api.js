e8fe0fb256fffd7c7fbc2fbac2c78200667046c2
yckj3156 <zhaojunjie@cloudwalk.com>
2020-10-10 14:03:47 +0800

    toast弹出调整

--- utils/server/api.js
+++ utils/server/api.js
@@ -1,31 +1,31 @@
 // loading配置，请求次数统计
-function startLoading() {
-    wx.showLoading({
-        title: 'Loading...',
-        icon: 'none'
-    })
-}
+// function startLoading() {
+//     wx.showLoading({
+//         title: 'Loading...',
+//         icon: 'none'
+//     })
+// }
 
-function endLoading() {
-    wx.hideLoading();
-}
+// function endLoading() {
+//     wx.hideLoading();
+// }
 // 声明一个对象用于存储请求个数
-let needLoadingRequestCount = 0;
+// let needLoadingRequestCount = 0;
 
-function showFullScreenLoading() {
-    if (needLoadingRequestCount === 0) {
-        startLoading();
-    }
-    needLoadingRequestCount++;
-};
+// function showFullScreenLoading() {
+//     if (needLoadingRequestCount === 0) {
+//         startLoading();
+//     }
+//     needLoadingRequestCount++;
+// };
 
-function tryHideFullScreenLoading() {
-    if (needLoadingRequestCount <= 0) return;
-    needLoadingRequestCount--;
-    if (needLoadingRequestCount === 0) {
-        endLoading();
-    }
-};
+// function tryHideFullScreenLoading() {
+//     if (needLoadingRequestCount <= 0) return;
+//     needLoadingRequestCount--;
+//     if (needLoadingRequestCount === 0) {
+//         endLoading();
+//     }
+// };
 
 const Api = {
     request: function (method, url, params) {
@@ -49,20 +49,27 @@ const Api = {
                     } = res;
                     if (res.statusCode === 200) {
                         if (data.code !== 1 && ( !params  || !params.notShowError)) {
-                            console.log("reponse==>",res)
-                            wx.showToast({
-                                title: data.msg,
-                                icon: 'none',
-                                duration: 2000,
-                            })
+                            let timer = setTimeout(() => {
+                                clearTimeout(timer);
+                                timer = null;
+                                wx.showToast({
+                                    title: data.msg,
+                                    icon: 'none',
+                                    duration: 2000,
+                                })
+                            }, 300);
                         } 
                         resolve(res.data)
                     } else {
-                        wx.showToast({
-                            title: `服务器异常 - ${statusCode}`,
-                            icon: 'none',
-                            duration: 2000
-                        })
+                        let timer = setTimeout(() => {
+                            clearTimeout(timer);
+                            timer = null;
+                            wx.showToast({
+                                title: `服务器异常 - ${statusCode}`,
+                                icon: 'none',
+                                duration: 2000
+                            })
+                        }, 300);
                         resolve(res)
                     }
                 },
